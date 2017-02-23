ace4eb05b5aa771c238186d4d1b5c99265ce07a3
Harman <hsahota93@yahoo.com>
2017-02-22 19:33:53 -0500

    Initial commit Start of something wonderful.

---
+++ desktop/src/com/hj/daggeroftime/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hj.daggeroftime.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hj.daggeroftime.DaggerOfTIme;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DaggerOfTIme(), config);
+	}
+}
